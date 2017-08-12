59305671d4ee0cbf15d96e7316bcfc0b57d6cbc7
Ellllll <583442185@qq.com>
2017-08-12 20:05:52 +0800

    加入sqlite

--- App1/App1.Android/MainActivity.cs
+++ App1/App1.Android/MainActivity.cs
@@ -7,19 +7,22 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Android.Content;
+using SQLite;
+using System.IO;
 
 namespace App1.Droid
 {
 	[Activity (Theme="@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
-		protected override void OnCreate (Bundle bundle)
+        protected override void OnCreate (Bundle bundle)
 		{
 			TabLayoutResource = Resource.Layout.Tabbar;
 			ToolbarResource = Resource.Layout.Toolbar; 
 
 			base.OnCreate (bundle);
 
+            sqliteConn = new SQLiteConnection(dbPath);
 			global::Xamarin.Forms.Forms.Init (this, bundle);
 			LoadApplication (new App1.App ());
 		}
--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -1,18 +1,88 @@
-﻿using System;
+﻿
+using SQLite;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace App1
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new TryEventBindingPageViewModel();
+
+        }
+
+        
+
+    }
+
+    [Table("UserInfo")]
+    public class UserInfo
+    {
+        [PrimaryKey, AutoIncrement, Collation("Id")]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Pwd { get; set; }
+    }
+
+    public class TryEventBindingPageViewModel
+    {
+        static string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "userinfo.db3");
+        private const string TableName = "UserInfo";
+        private ICommand _buttonClickCommand ;
+        public ICommand buttonClickCommand
+        {
+            get { return _buttonClickCommand; }
+            set { _buttonClickCommand = new Command(ButtonClickCommandExecute, CanButtonClickCommandExecute); }
+        }
+        private string userNameText;
+        public string UserNameText { get {return userNameText; } set {userNameText=value; } }
+        private string pwdText;
+        public string PwdText { get { return pwdText; } set { pwdText = value; } }
+
+        //login
+        private void ButtonClickCommandExecute(object args)
+        {
+            var UserName = userNameText;
+            var Pwd = pwdText;
+            Login(UserName,Pwd);
+        }
+
+
+        private bool CanButtonClickCommandExecute(object args)
+        {
+            return true;
+        }
+
+        private void Login(string userName,string pwd)
+        {
+            var sqliteConn=new Sqlite1();
+            var userInfoTable = sqliteConn.GetTableInfo(TableName);
+            if (userInfoTable.Count == 0)
+            {
+                sqliteConn.CreateTable<UserInfo>();
+            }
+            var userInfos = sqliteConn.Table<UserInfo>();
+            var userInfo = userInfos.Where(p => p.Pwd == pwd && p.UserName == userName).FirstOrDefault();
+            if (userInfo == null)
+            {
+                Android.Widget.Toast.MakeText(this, "用户名或密码不正确", Android.Widget.ToastLength.Short).Show();
+            }
+            else
+            {
+                Android.Widget.Toast.MakeText(this, "登录成功", Android.Widget.ToastLength.Short).Show();
+            }
+
+
+        }
+    }
 
 }
